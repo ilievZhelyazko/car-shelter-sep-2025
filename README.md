85c40bb2ab75e4e7057229b5bf1a22c229fb1589
ilievZhelyazko <pishtoran@gmail.com>
2025-10-07 13:40:01 +0300

    show home page

--- README.md
+++ README.md
@@ -5,4 +5,4 @@ softUni js back end Course project
 ## TODOs
 
 - [x] Initiale NodeJS Project
-  - [x] dev enviroment - [x] dev scrypt - [x] Debugging -[x] Add basic http server -[x] add resources
+  - [x] dev enviroment - [x] dev scrypt - [x] Debugging -[x] Add basic http server -[x] add resources -[x] show homepage
--- src/index.js
+++ src/index.js
@@ -1,9 +1,16 @@
 import http from "http";
-
+import homeHTML from "./home.html.js";
+import siteCSS from "./site.css.js";
 const server = http.createServer((req, res) => {
-  res.write("Hello word!!!");
+  if (req.url === "/") {
+    res.writeHead(200, { "Content-Type": "text/html" });
 
+    res.write(homeHTML);
+  } else if (req.url === "/styles/site.css") {
+    res.writeHead(200, { "content-type": "text/css" });
+    res.write(siteCSS);
+  }
   res.end();
 });
 server.listen(5000);
-console.log("Server is listen on http://localhost5000.... ");
+console.log("Server is listening on http://localhost:5000....");
