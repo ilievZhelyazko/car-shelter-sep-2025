12b0650e1e263150fde4e46187772ae48e871c94
ilievZhelyazko <pishtoran@gmail.com>
2025-10-07 14:13:15 +0300

    read htnl from file system

--- README.md
+++ README.md
@@ -5,4 +5,7 @@ softUni js back end Course project
 ## TODOs
 
 - [x] Initiale NodeJS Project
-  - [x] dev enviroment - [x] dev scrypt - [x] Debugging -[x] Add basic http server -[x] add resources -[x] show homepage
+- [x] dev enviroment
+- [x] dev scrypt
+- [x] Debugging -[x] Add basic http server -[x] add resources -[x] show homepage
+      -[] Read home html from file
--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
 import http from "http";
-import homeHTML from "./home.html.js";
+import fs from "fs/promises";
+import path from "path";
 import siteCSS from "./site.css.js";
-const server = http.createServer((req, res) => {
+const server = http.createServer(async (req, res) => {
   if (req.url === "/") {
+    const homeHtml = await fs.readFile("./src/views/home/index.html", {
+      encoding: "utf-8",
+    });
     res.writeHead(200, { "Content-Type": "text/html" });
 
-    res.write(homeHTML);
+    res.write(homeHtml);
   } else if (req.url === "/styles/site.css") {
     res.writeHead(200, { "content-type": "text/css" });
     res.write(siteCSS);
