209058d9c28bf1c7e64a52bd44d2dd209440e896
ilievZhelyazko <pishtoran@gmail.com>
2025-10-10 09:45:22 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -20,9 +20,7 @@ const server = http.createServer(async (req, res) => {
       html = await addCatView();
       break;
     case "/styles/site.css":
-      const siteCss = await fs.readFile("./src/styles/site.css", {
-        encoding: "utf-8",
-      });
+      const siteCss = await readFile("./src/styles/site.css");
 
       res.write(siteCss);
       res.end();
@@ -34,11 +32,11 @@ const server = http.createServer(async (req, res) => {
   res.write(html);
   res.end();
 });
-function renderView(path) {
+function readFile(path) {
   return fs.readFile(path, { encoding: "utf-8" });
 }
 async function homeView() {
-  const homeHtml = renderView("./src/views/home/index.html");
+  const homeHtml = readFile("./src/views/home/index.html");
   return homeHtml;
 }
 async function addBreedView() {
@@ -48,7 +46,7 @@ async function addBreedView() {
   return html;
 }
 async function addCatView() {
-  const html = await renderView("./src/views/addCat.html");
+  const html = await readFile("./src/views/addCat.html");
   return html;
 }
 server.listen(5000);
